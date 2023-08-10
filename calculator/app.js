1273667f1586c140667a8377beb55ece5c9ca185
meerweeN <141247784+meerweeN@users.noreply.github.com>
2023-08-10 21:30:28 +0300

    Add files via upload

---
+++ calculator/app.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.calculatorDisplay');
+const operators = ["/","*","+","-"];
+let output ="";
+
+const calculate = (btnValue) => {
+    if (btnValue === "=" && output !==""){
+        output= eval(output);
+
+    } else if (btnValue =='C') {
+        output = "";
+    }
+    else {
+         if(output === ""&& operators.includes(btnValue)) return;
+         output +=btnValue;
+    }
+
+    display.value = output;
+}
+
+
+buttons.forEach(button => {
+    button.addEventListener('click',(d) =>calculate(d.target.dataset.value))
+})
