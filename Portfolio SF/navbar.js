bd4986c9a156ed0d93dfa65d9fd4152130e898aa
Flavius Stanciu <141247784+meerweeN@users.noreply.github.com>
2023-09-15 12:27:19 +0300

    Update navbar.js

--- Portfolio SF/navbar.js
+++ Portfolio SF/navbar.js
@@ -2,12 +2,19 @@ const body = document.querySelector("body");
 const navbar = document.querySelector(".navbar");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
+const itemList = document.querySelector(".menu-list");
 menuBtn.onclick = ()=>{
   navbar.classList.add("show");
   menuBtn.classList.add("hide");
   body.classList.add("disabled");
 }
 cancelBtn.onclick = ()=>{
+  body.classList.remove("disabled");
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+}
+
+itemList.onclick = ()=>{
   body.classList.remove("disabled");
   navbar.classList.remove("show");
   menuBtn.classList.remove("hide");
