e44144acb0fd419cfdb95d3cb374b0a6c7c23923
Flavius Stanciu <141247784+meerweeN@users.noreply.github.com>
2023-09-17 17:31:08 +0300

    code format

--- Mini Projects/todo-list/app.js
+++ Mini Projects/todo-list/app.js
@@ -2,19 +2,17 @@ let addButton = document.getElementById("addButton");
 let inputText = document.getElementById("inputBox");
 let toDoText = document.getElementById("exitText");
 
+addButton.addEventListener("click", function () {
+  let addParagraph = document.createElement("p");
+  toDoText.appendChild(addParagraph);
+  addParagraph.innerText = inputText.value;
+  inputText.value = "";
 
-addButton.addEventListener("click",function(){
-    let addParagraph = document.createElement('p');
-    toDoText.appendChild(addParagraph);
-    addParagraph.innerText = inputText.value;
-    inputText.value = "";
+  addParagraph.addEventListener("click", function () {
+    addParagraph.style.textDecoration = "line-through";
+  });
 
-        addParagraph.addEventListener("click",function(){
-            addParagraph.style.textDecoration = "line-through";
-        })
-
-        addParagraph.addEventListener("dblclick",function(){
-            toDoText.removeChild(addParagraph);
-        })
+  addParagraph.addEventListener("dblclick", function () {
+    toDoText.removeChild(addParagraph);
+  });
 });
-
