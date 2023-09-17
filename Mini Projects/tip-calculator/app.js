84bf3ed80eb5d3c8fe19e5d60b2ac838d4ae0f9e
Flavius Stanciu <141247784+meerweeN@users.noreply.github.com>
2023-09-17 17:30:17 +0300

    format code

--- Mini Projects/tip-calculator/app.js
+++ Mini Projects/tip-calculator/app.js
@@ -3,36 +3,23 @@ let tipAmount = document.getElementById("tip");
 let exitText = document.getElementById("exitText");
 let calculateBtn = document.getElementById("tipBtn");
 
-calculateBtn.addEventListener("click", function(){
+calculateBtn.addEventListener("click", function () {
+  let billTotal = (+tipAmount.value / 100) * bill.value + +bill.value;
 
-    let billTotal = (+tipAmount.value/100) * bill.value + +bill.value;
-
-   
-        if(oneBox.checked) {
-        exitText.innerHTML = ("Total is " + billTotal + "$");
-    }
-
-        else if (twoBox.checked) {  
-        exitText.innerHTML = ("Total is " +billTotal/2 + "$");
-    }
-
-        else if (threeBox.checked) {  
-        exitText.innerHTML = ("Total is " +billTotal/3 + "$");
-     }
-
-        else if (fourBox.checked) {  
-        exitText.innerHTML = ("Total is " +billTotal/4 + "$");
-     }
-        else if (fiveBox.checked) {  
-        exitText.innerHTML = ("Total is " +billTotal/5 + "$");
-     }
-       else if (sixBox.checked) {
-        exitText.innerHTML = ("Total is " +billTotal/6 + "$");
-       }
-       else {
-        exitText.innerHTML = ("Please select the number of persons to split between!");
-       }
-
+  if (oneBox.checked) {
+    exitText.innerHTML = "Total is " + billTotal + "$";
+  } else if (twoBox.checked) {
+    exitText.innerHTML = "Total is " + billTotal / 2 + "$";
+  } else if (threeBox.checked) {
+    exitText.innerHTML = "Total is " + billTotal / 3 + "$";
+  } else if (fourBox.checked) {
+    exitText.innerHTML = "Total is " + billTotal / 4 + "$";
+  } else if (fiveBox.checked) {
+    exitText.innerHTML = "Total is " + billTotal / 5 + "$";
+  } else if (sixBox.checked) {
+    exitText.innerHTML = "Total is " + billTotal / 6 + "$";
+  } else {
+    exitText.innerHTML =
+      "Please select the number of persons to split between!";
+  }
 });
-
- 
